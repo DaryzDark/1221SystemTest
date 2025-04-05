4ec0d4902e601e393bcbdc489561ee9cc615bd5e
daryz <daryz.dark@gmail.com>
2025-04-05 18:51:07 +0700

    Initial commit

---
+++ src/test/java/com/_Systems/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com._Systems.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
